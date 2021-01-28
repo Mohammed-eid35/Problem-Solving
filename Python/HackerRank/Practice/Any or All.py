fee34e53ff9fbc66467e844649a575e53db14146
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-01-28 16:51:32 +0200

    Create Any or All.py

---
+++ Python/HackerRank/Practice/Any or All.py
@@ -0,0 +1,5 @@
+# Link : https://www.hackerrank.com/challenges/any-or-all/problem
+
+n = int(input())
+lst = input().split(" ")
+print(all(int(i) >= 0 for i in lst) and any(i == i[::-1]for i in lst))
