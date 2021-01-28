23334b41e0c16c8576a8b72589917f841b2e8bb2
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-01-28 16:02:07 +0200

    Create C - Simple Calculator.cpp

---
+++ C++/codeforces/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/C - Simple Calculator.cpp
@@ -0,0 +1,20 @@
+//:::: Alien :::://
+// Muhammad Eid //
+#include <bits/stdc++.h>
+using namespace std;
+
+void Mo35() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {  Mo35();
+
+    long long x, y; cin >> x >> y;
+    cout << x << " + " << y << " = " << (x + y) << '\n';
+    cout << x << " * " << y << " = " << (x * y) << '\n';
+    cout << x << " - " << y << " = " << (x - y) << '\n';
+
+return 0;
+}
