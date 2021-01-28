774fe20b2cb8fbb3231d6fd0f04b15da9d1325e7
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-01-28 16:08:13 +0200

    Create E. Area of a Circle.cpp

---
+++ C++/codeforces/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/E. Area of a Circle.cpp
@@ -0,0 +1,19 @@
+//:::: Alien :::://
+// Muhammad Eid //
+#include <bits/stdc++.h>
+using namespace std;
+
+void Mo35() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {  Mo35();
+
+    const double PI = 3.141592653;
+    double r; cin >> r;
+    cout << fixed << setprecision(9) << (PI * r * r);
+
+return 0;
+}
