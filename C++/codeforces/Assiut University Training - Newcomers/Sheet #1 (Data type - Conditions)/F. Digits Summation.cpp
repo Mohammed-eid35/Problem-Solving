edf72027cd231dff3e0a23a9ebf8803be2787887
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-01-28 16:12:06 +0200

    Create F. Digits Summation.cpp

---
+++ C++/codeforces/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/F. Digits Summation.cpp
@@ -0,0 +1,18 @@
+//:::: Alien :::://
+// Muhammad Eid //
+#include <bits/stdc++.h>
+using namespace std;
+
+void Mo35() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {  Mo35();
+
+    long long n, m; cin >> n >> m;
+    cout << (n % 10) + (m % 10);
+
+return 0;
+}
