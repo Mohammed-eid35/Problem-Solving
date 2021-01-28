a0e4f228cedb09a457db9971b58969063bb36fbd
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-01-28 10:07:30 +0200

    Create Loops.py

---
+++ Python/HackerRank/Practice/Loops.py
@@ -0,0 +1,13 @@
+# Link : https://www.hackerrank.com/challenges/python-loops/problem
+
+# 1
+n = int(input())
+for i in range(n) :
+    print(i * i)
+
+
+# 2
+import math
+n = int(input())
+for i in range(n) :
+	print(int(math.pow(i, 2)))
