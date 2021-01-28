d0ab323cbfe02c43be4b58c4f1802dfa7f6a7173
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-01-28 15:58:51 +0200

    Create B - Basic Data Types.cpp

---
+++ C++/codeforces/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/B - Basic Data Types.cpp
@@ -0,0 +1,19 @@
+//:::: Alien :::://
+// Muhammad Eid //
+#include <bits/stdc++.h>
+using namespace std;
+
+void Mo35() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {  Mo35();
+
+    int a; long long b; char c; float d; double e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a << '\n' << b << '\n' << c << '\n' << d << '\n' << e;
+
+return 0;
+}
