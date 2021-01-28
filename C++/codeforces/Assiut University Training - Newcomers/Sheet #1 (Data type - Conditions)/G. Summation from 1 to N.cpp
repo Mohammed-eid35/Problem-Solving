53329ec675645561c6796c96615a4724d44b3bc9
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-01-28 16:16:31 +0200

    Create G. Summation from 1 to N.cpp

---
+++ C++/codeforces/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/G. Summation from 1 to N.cpp
@@ -0,0 +1,18 @@
+//:::: Alien :::://
+// Muhammad Eid //
+#include <bits/stdc++.h>
+using namespace std;
+
+void Mo35() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {  Mo35();
+
+    long long n; cin >> n;
+    cout << n * (n + 1) / 2;
+
+return 0;
+}
