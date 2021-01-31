bc44a32efcfeb84618142342c8ce35572cfd2908
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-01-31 14:04:16 +0200

    Create I. Welcome for you with Conditions.cpp

---
+++ C++/codeforces/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/I. Welcome for you with Conditions.cpp
@@ -0,0 +1,18 @@
+//:::: Alien :::://
+// Muhammad Eid //
+#include <bits/stdc++.h>
+using namespace std;
+
+void Mo35() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {  Mo35();
+
+    int a, b; cin >> a >> b;
+    cout << (a >= b ? "Yes" : "No");
+
+return 0;
+}
