27715891a9c55410a3672950ac87ebd80b448b53
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-01-31 14:14:43 +0200

    Create L. The Brothers.cpp

---
+++ C++/codeforces/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/L. The Brothers.cpp
@@ -0,0 +1,19 @@
+//:::: Alien :::://
+// Muhammad Eid //
+#include <bits/stdc++.h>
+using namespace std;
+
+void Mo35() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {  Mo35();
+
+    string name1; cin >> name1 >> name1;
+    string name2; cin >> name2 >> name2;
+    cout << (name1 == name2 ? "ARE Brothers" : "NOT");
+
+return 0;
+}
