ddbec47c12b4ed341b0c38845389bf9002a57235
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-01-31 13:59:54 +0200

    Create H. Two numbers.cpp

---
+++ C++/codeforces/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/H. Two numbers.cpp
@@ -0,0 +1,20 @@
+//:::: Alien :::://
+// Muhammad Eid //
+#include <bits/stdc++.h>
+using namespace std;
+
+void Mo35() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {  Mo35();
+
+    double a, b; cin >> a >> b;
+    cout << "floor " << a << " / " << b << " = " << floor(a / b) << '\n';
+    cout << "ceil " << a << " / " << b << " = " << ceil(a / b) << '\n';
+    cout << "round " << a << " / " << b << " = " << round(a / b);
+
+return 0;
+}
