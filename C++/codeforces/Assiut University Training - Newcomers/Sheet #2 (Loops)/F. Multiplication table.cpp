a208a6da27736918464448afd9d6ba97ae304ea0
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-02-03 16:20:18 +0200

    Create F. Multiplication table.cpp

---
+++ C++/codeforces/Assiut University Training - Newcomers/Sheet #2 (Loops)/F. Multiplication table.cpp
@@ -0,0 +1,19 @@
+//:::: Alien :::://
+// Muhammad Eid //
+#include <bits/stdc++.h>
+using namespace std;
+
+void Mo35() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {  Mo35();
+
+    int n; cin >> n;
+    for(int i=1; i<=12; ++i)
+        cout << n << " * " << i << " = " << n*i << '\n';
+
+return 0;
+}
