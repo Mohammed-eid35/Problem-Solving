9bd2cf4617a35d8ec950b1954c4c653ccce39d08
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-02-03 17:27:17 +0200

    Create W. Shape3.cpp

---
+++ C++/codeforces/Assiut University Training - Newcomers/Sheet #2 (Loops)/W. Shape3.cpp
@@ -0,0 +1,33 @@
+//:::: Alien :::://
+// Muhammad Eid //
+#include <bits/stdc++.h>
+using namespace std;
+
+void Mo35() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {  Mo35();
+
+    int n; cin >> n;
+    for(int i=1; i<=n; ++i) {
+        for(int j=0; j<n-i; ++j)
+            cout << ' ';
+
+        for(int j=1; j<i; ++j)
+            cout << "**";
+        cout << "*\n";
+    }
+    for(int i=n; i>=1; --i) {
+        for(int j=0; j<n-i; ++j)
+            cout << ' ';
+
+        for(int j=1; j<i; ++j)
+            cout << "**";
+        cout << "*\n";
+    }
+
+return 0;
+}
