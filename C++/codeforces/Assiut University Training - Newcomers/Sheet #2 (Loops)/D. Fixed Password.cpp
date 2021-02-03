812977a884ce1cb7ca9058fa8c649e9da863dc17
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-02-03 16:15:38 +0200

    Create D. Fixed Password.cpp

---
+++ C++/codeforces/Assiut University Training - Newcomers/Sheet #2 (Loops)/D. Fixed Password.cpp
@@ -0,0 +1,24 @@
+//:::: Alien :::://
+// Muhammad Eid //
+#include <bits/stdc++.h>
+using namespace std;
+
+void Mo35() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {  Mo35();
+
+    int x;
+    while(cin >> x)
+        if(x == 1999) {
+            cout << "Correct";
+            break;
+        }
+        else
+            cout << "Wrong\n";
+
+return 0;
+}
