0889bef4399f6fc5473f2911cf24c85b7db382a1
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-02-06 10:45:35 +0200

    Create abc190_a Very Very Primitive Game.cpp

---
+++ C++/atcoder/abc190_a Very Very Primitive Game.cpp
@@ -0,0 +1,19 @@
+/// Link : https://atcoder.jp/contests/abc190/tasks/abc190_a
+//:::: Alien :::://
+// Muhammad Eid //
+#include <bits/stdc++.h>
+using namespace std;
+
+void Mo35() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {  Mo35();
+
+    int a, b, c; cin >> a >> b >> c;
+    cout << (a + c > b ? "Takahashi" : "Aoki");
+
+return 0;
+}
