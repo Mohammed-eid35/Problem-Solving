15114878c3d675e44f2c9dfa6bb0cbecd64de03d
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-02-06 10:24:02 +0200

    Create 734A - Anton and Danik.cpp

---
+++ C++/codeforces/734A - Anton and Danik.cpp
@@ -0,0 +1,33 @@
+/// Link : 
+//:::: Alien :::://
+// Muhammad Eid //
+#include <bits/stdc++.h>
+using namespace std;
+
+void Mo35() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {  Mo35();
+
+    int n; cin >> n;
+    char c;
+    int d = 0, a = 0;
+    while(n--) {
+        cin >> c;
+        if(c == 'A')
+            a++;
+        else
+            d++;
+    }
+    if(a > d)
+        cout << "Anton";
+    else if(d > a)
+        cout << "Danik";
+    else
+        cout << "Friendship";
+
+return 0;
+}
