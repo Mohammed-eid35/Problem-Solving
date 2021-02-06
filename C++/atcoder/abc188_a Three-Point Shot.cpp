ecaf8c2ec1e72caf648cae3793e4502c9aad7bfe
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-02-06 11:27:54 +0200

    Create abc188_a Three-Point Shot.cpp

---
+++ C++/atcoder/abc188_a Three-Point Shot.cpp
@@ -0,0 +1,19 @@
+/// Link : https://atcoder.jp/contests/abc188/tasks/abc188_a
+//:::: Alien :::://
+// Muhammad Eid //
+#include <bits/stdc++.h>
+using namespace std;
+
+void Mo35() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {  Mo35();
+
+    int x, y; cin >> x >> y;
+    cout << (abs(x - y) < 3 ? "Yes" : "No");
+
+return 0;
+}
