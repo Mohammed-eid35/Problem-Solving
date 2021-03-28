dbed7b6ca83998581080aebe88141bd26224c66e
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-03-28 22:05:27 +0200

    Create abc043_a Children and Candies.cpp

---
+++ C++/atcoder/abc043_a Children and Candies.cpp
@@ -0,0 +1,19 @@
+//:::: Alien :::://
+// Muhammad Eid //
+#include <bits/stdc++.h>
+using namespace std;
+
+void Mo35() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {  Mo35();
+
+    int n;
+    cin >> n;
+    cout << n * (n + 1) / 2;
+
+return 0;
+}
