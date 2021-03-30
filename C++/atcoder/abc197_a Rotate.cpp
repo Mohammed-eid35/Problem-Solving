92fec57d322e0d9a981e895ab72431e5f68fe18a
Muhammad Eid <71411379+Mohammed-eid35@users.noreply.github.com>
2021-03-30 13:41:12 +0200

    Create abc197_a Rotate.cpp

---
+++ C++/atcoder/abc197_a Rotate.cpp
@@ -0,0 +1,19 @@
+//:::: Alien :::://
+// Muhammad Eid //
+#include <bits/stdc++.h>
+using namespace std;
+
+void Mo35() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main() {  Mo35();
+
+    string s; 
+    cin >> s;
+    cout << s.substr(1, s.size() - 1) << s[0];
+
+return 0;
+}
