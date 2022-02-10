13c499d97d2f87be0e34bb25d6795fe14130f147
Alien <71411379+Mohammed-eid35@users.noreply.github.com>
2022-02-10 14:45:29 +0200

    Update UPDATEIT - Update the array !.cpp

--- spoj/UPDATEIT - Update the array !.cpp
+++ spoj/UPDATEIT - Update the array !.cpp
@@ -3,45 +3,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void Mo35() {
+void Alien35() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
 #ifndef ONLINE_JUDGE
-    freopen("input.txt", "rt", stdin);
-    //freopen("output.txt", "wt", stdout);
+    freopen("test.in", "rt", stdin);
+    freopen("test.out", "wt", stdout);
 #endif
 }
 
-const int N = 1e4 + 5;
-int ar[N];
+const int N = 100 + 5, M = 1e4 + 5, OO = 0x3f3f3f3f, MOD = 1e9 + 7;
 
 void test() {
-    int n, u, l, r, val;
-    cin >> n >> u;
-    memset(ar, 0, n * sizeof(ar[0]));
+    int n, u; cin >> n >> u;
+    vector<int> vc(n + 1);
     while (u--) {
-        cin >> l >> r >> val;
-        ar[l] += val;
-        ar[r+1] -= val;
+        int st, en, val; cin >> st >> en >> val;
+        vc[st] += val;
+        vc[en + 1] -= val;
     }
-    for (int i = 1; i < n; ++i)
-        ar[i] += ar[i-1];
-    int q, idx;
-    cin >> q;
+    for (int i = 1; i < n; ++i) {
+        vc[i] += vc[i - 1];
+    }
+    int q; cin >> q;
     while (q--) {
-        cin >> idx;
-        cout << ar[idx] << '\n';
+        int idx; cin >> idx;
+        cout << vc[idx] << '\n';
     }
 }
 
-int main() {  Mo35();
+int main() {  Alien35();
 
-    int t;
-    cin >> t;
-    while (t--)
+    int t; cin >> t;
+    while (t--) {
         test();
+    }
 
 return 0;
 }
