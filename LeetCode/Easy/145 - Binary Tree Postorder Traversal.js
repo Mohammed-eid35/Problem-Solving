7efc17967c2ef5ac2fb5f7bd3ccd52106a01c0a0
Alien <71411379+Mohammed-eid35@users.noreply.github.com>
2022-05-22 10:43:45 +0200

    Create 145 - Binary Tree Postorder Traversal.js

---
+++ LeetCode/Easy/145 - Binary Tree Postorder Traversal.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    const ret = [];
+    
+    const solve = (cur) => {
+        if (!cur) return;
+        
+        solve(cur.left);
+        solve(cur.right);
+        ret.push(cur.val);
+    }
+    solve(root);
+    
+    return ret;
+};
