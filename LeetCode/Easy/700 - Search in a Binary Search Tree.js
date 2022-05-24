460ef0831e3c0d6d9d4dfa3cef4600118d8ef072
Alien <71411379+Mohammed-eid35@users.noreply.github.com>
2022-05-24 10:01:46 +0200

    Create 700 - Search in a Binary Search Tree.js

---
+++ LeetCode/Easy/700 - Search in a Binary Search Tree.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    
+    while (root && root.val != val) {
+        root = val < root.val ? root.left : root.right;
+    }
+    
+    return root;
+};
