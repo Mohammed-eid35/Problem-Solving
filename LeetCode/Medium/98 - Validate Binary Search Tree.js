bd03308adddf817c06c07cf0df032c6b0dbc8b1e
Alien <71411379+Mohammed-eid35@users.noreply.github.com>
2022-05-25 10:28:59 +0200

    Create 98 - Validate Binary Search Tree.js

---
+++ LeetCode/Medium/98 - Validate Binary Search Tree.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root, min = -Infinity, max = Infinity) {
+    if (!root) return true;
+    if (root.val <= min || root.val >= max) return false;
+    
+    return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+};
