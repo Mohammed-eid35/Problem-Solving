f4fb897d06913b150c39da25c92abf95bb66166a
Alien <71411379+Mohammed-eid35@users.noreply.github.com>
2022-06-01 12:32:49 +0200

    Create 876 - Middle of the Linked List.js

---
+++ LeetCode/Easy/876 - Middle of the Linked List.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    const ar = [];
+    while (head) {
+        ar.push(head)
+        head = head.next;
+    }
+    return ar[Math.floor(ar.length / 2)];
+};
