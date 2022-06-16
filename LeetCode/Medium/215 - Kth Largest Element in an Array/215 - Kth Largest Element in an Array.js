355aa8e731231388091f772966aef2ed7a75dda6
Mohammed-eid35 <meid95874@gmail.com>
2022-06-16 23:08:59 +0200

    Create 215 - Kth Largest Element in an Array.js

---
+++ LeetCode/Medium/215 - Kth Largest Element in an Array/215 - Kth Largest Element in an Array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+ var findKthLargest = function(nums, k) {
+    nums.sort((a, b) => b - a);
+    
+    return nums[k - 1];
+};
