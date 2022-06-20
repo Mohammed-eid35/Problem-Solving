8168e4bfbe5ed710391ad31b4f030a038e89082c
Mohammed-eid35 <meid95874@gmail.com>
2022-06-21 00:42:29 +0200

    Create list-comprehensions-2.py

---
+++ HackerRank/Python/list-comprehensions/list-comprehensions-2.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    res = []
+    for i in range(x + 1):
+        for j in range(y + 1):
+            for k in range(z + 1):
+                if i + j + k != n:
+                    res.append([i, j, k])
+    print(res)
