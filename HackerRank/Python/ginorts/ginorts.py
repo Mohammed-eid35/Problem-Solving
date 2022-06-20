6c93000c2ff92cf3a035aa54fc90d0da5c3f4667
Mohammed-eid35 <meid95874@gmail.com>
2022-06-21 01:10:48 +0200

    Create ginorts.py

--- HackerRank/Python/ginorts/ginorts.py
+++ HackerRank/Python/ginorts/ginorts.py
@@ -1,8 +1,8 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-def keyFunction(x):
+def key_function(x):
     if x.islower(): return (1, x)
     if x.isupper(): return (2, x)
     if int(x) & 1 : return (3, x)
     return (4, x)
 
-print(''.join(sorted(input(), key = keyFunction)))
+print(''.join(sorted(input(), key = key_function)))
