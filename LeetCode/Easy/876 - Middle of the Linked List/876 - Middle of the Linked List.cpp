02389fb8912fc6b58e19287f2335703872874ee0
Mohammed-eid35 <meid95874@gmail.com>
2022-06-27 00:28:00 +0200

    Create 876 - Middle of the Linked List.cpp

---
+++ LeetCode/Easy/876 - Middle of the Linked List/876 - Middle of the Linked List.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *ret = head;
+        while (head && head->next) {
+            head = head->next->next;
+            ret = ret->next;
+        }
+        return ret;
+    }
+};
