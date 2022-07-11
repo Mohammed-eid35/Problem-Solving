1ce22d8097e5f67b36cb4932e4fe61bdf775a7ef
Mohammed-eid35 <meid95874@gmail.com>
2022-07-11 21:22:42 +0200

    Create 162 - Find Peak Element.cpp

---
+++ LeetCode/Medium/162 - Find Peak Element/162 - Find Peak Element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int lo = 0, hi = nums.size() - 1;
+        while (lo < hi) {
+            int mid = lo + ((hi - lo) >> 1);
+            if (nums[mid] > nums[mid + 1])
+                hi = mid;
+            else
+                lo = mid + 1;
+        }
+        return lo;
+    }
+};
