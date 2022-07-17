52f7b967c2d3d9fd729041e1dd8159f4ede4354a
Mohammed-eid35 <meid95874@gmail.com>
2022-07-17 07:19:27 +0200

    Create 797 - All Paths From Source to Target.cpp

---
+++ LeetCode/Medium/797 - All Paths From Source to Target/797 - All Paths From Source to Target.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    int n;
+    vector<bool> stk;
+    vector<vector<int>> paths;
+    vector<int> path = {0};
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        n = graph.size();
+        stk = vector<bool>(n);
+        DFS(graph, 0);
+        return paths;
+    }
+    
+    void DFS(vector<vector<int>> graph, int u) {
+        if (u == n - 1) {
+            paths.push_back(path);
+            return;
+        }
+        
+        for (auto v : graph[u]) {
+            if (stk[v]) continue;
+            
+            path.push_back(v);
+            stk[v] = true;
+            
+            DFS(graph, v);
+            
+            path.pop_back();
+            stk[v] = false;
+        }
+    }
+};
